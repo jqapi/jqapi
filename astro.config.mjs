4d0bf3628329e78df7f4530d3677f47b8d464a5e
Sebastian Senf <contact@mustardamus.com>
2023-01-15 16:09:43 +0100

    added astro-purgecss tostrip unused css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from "astro/config";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    purgecss({
+      safelist: ["html", "body", "selected", "active", "notfound"],
+    }),
+  ],
+});
