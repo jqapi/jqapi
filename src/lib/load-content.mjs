83ed0f3341049d965a066bd318459dea35fb3c1c
Sebastian Senf <contact@mustardamus.com>
2023-01-16 12:01:35 +0100

    remove trailing slash of href to load

--- src/lib/load-content.mjs
+++ src/lib/load-content.mjs
@@ -1,6 +1,10 @@
 import $ from "jquery";
 
 export default function (href, pushState = true) {
+  if (href.charAt(href.length - 1) === "/") {
+    href = href.substr(0, href.length - 1);
+  }
+
   $("#content").load(`${href}[ajax] #content > *`, () => {
     if (pushState) {
       window.history.pushState({}, "", href);
