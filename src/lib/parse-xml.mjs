7cd73623f5176c8978f8333071154dd01f9239d4
Sebastian Senf <contact@mustardamus.com>
2023-01-21 11:32:25 +0100

    expand all includes when parsing xml

--- src/lib/parse-xml.mjs
+++ src/lib/parse-xml.mjs
@@ -1,9 +1,23 @@
 import { readFile } from "node:fs/promises";
 import $ from "./jquery.mjs";
+import getFullPath from "../lib/get-full-path.mjs";
 
 export default async function parseXML(xmlPath) {
   const xml = await readFile(xmlPath, "utf-8");
   const xmlDoc = $.parseXML(xml);
+  const $xml = $(xmlDoc);
+  const $includes = $xml.find("xi\\:include");
 
-  return $(xmlDoc);
+  for (let i = 0; i < $includes.length; i++) {
+    const $include = $($includes[i]);
+    const href = $include.attr("href");
+    const incPath = getFullPath(`api.jquery.com/includes/${href}`);
+    const incXML = await readFile(incPath, "utf-8");
+    const incDoc = $.parseXML(incXML);
+    const $argument = $(incDoc).find("> argument");
+
+    $include.replaceWith($argument);
+  }
+
+  return $xml;
 }
